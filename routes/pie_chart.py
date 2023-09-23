24f8fc6d5eedfea15d1c91d7693b9206611b52c1
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-09-23 23:38:22 +0800

    te

--- routes/pie_chart.py
+++ routes/pie_chart.py
@@ -26,19 +26,27 @@ def calculate_pie_chart_angles(data):
     # Calculate total investment in the portfolio
     instrument_list = [instrument["quantity"] * instrument["price"] for instrument in instruments]
     total_investment = sum(instrument_list)
-    minimum_investment = total_investment * 0.05
-    for instrument in range(len(instrument_list)):
-        if instrument_list[instrument] < minimum_investment:
-            instrument_list[instrument] = minimum_investment
-    total_investment = sum(instrument_list)
     proportions = [(instrument / total_investment) for instrument in instrument_list]
-    
+    unadjusted_proportions = []
+    temp = 0
+    for proportion in range(len(proportions)):
+        if proportions[proportion] < 0.05:
+            proportions[proportion] = 0.05
+            temp += 1
+        else:
+            unadjusted_proportions.append(proportions[proportion])
+    new_total = sum(unadjusted_proportions)
+    final = []
+    for proportion in unadjusted_proportions:
+        final.append((proportion / new_total) * (1-temp*0.05))
+    for i in range(temp):
+        final.append(0.05)
     # Sort instruments based on proportions in descending order
-    proportions = sorted(proportions, reverse=True)
+    final = sorted(final, reverse=True)
     # Calculate angles for the boundaries of the slices/arcs of the pie chart
     angles = [0.0]
     cumulative_angle = 0.0
-    for proportion in proportions:
+    for proportion in final:
         angle = proportion * 2 * math.pi
         cumulative_angle += angle
         angles.append(cumulative_angle)
