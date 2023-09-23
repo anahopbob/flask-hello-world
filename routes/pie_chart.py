9957b2c66696cbeb9584b50414e6fc0980e0911e
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-09-23 23:19:03 +0800

    test

--- routes/pie_chart.py
+++ routes/pie_chart.py
@@ -24,31 +24,25 @@ def calculate_pie_chart_angles(data):
     instruments = data["data"]
     
     # Calculate total investment in the portfolio
-    total_investment = [instrument["quantity"] * instrument["price"] for instrument in instruments]
-    print("TOTAL INVESTMENTS", total_investment)
-    # Calculate proportions (percentages) of each instrument's investment relative to the total
-    proportions = [(instrument["quantity"] * instrument["price"] / total_investment) * 100 for instrument in instruments]
+    instrument_list = [instrument["quantity"] * instrument["price"] for instrument in instruments]
+    total_investment = sum(instrument_list)
+    minimum_investment = total_investment * 0.05
+    for instrument in range(len(instrument_list)):
+        if instrument_list[instrument]["quantity"] * instrument_list[instrument]["price"] < minimum_investment:
+            instrument_list[instrument]["quantity"] = minimum_investment
+    total_investment = sum(instrument_list)
+    proportions = [(instrument / total_investment) * 100 for instrument in instrument_list]
     
     # Sort instruments based on proportions in descending order
-    sorted_instruments = [instrument for _, instrument in sorted(zip(proportions, instruments), reverse=True)]
-    minimum_investment = total_investment * 0.05
-    for instrument in range(len(sorted_instruments)):
-        if sorted_instruments[instrument]["quantity"] * sorted_instruments[instrument]["price"] < minimum_investment:
-            sorted_instruments[instrument]["quantity"] = minimum_investment / sorted_instruments[instrument]["price"]
+    proportions = sorted(proportions, reverse=True)
     # Calculate angles for the boundaries of the slices/arcs of the pie chart
     angles = [0.0]
     cumulative_angle = 0.0
-    for instrument in sorted_instruments:
-        proportion = (instrument["quantity"] * instrument["price"] / total_investment)
+    for proportion in proportions:
         angle = proportion * 2 * math.pi
         cumulative_angle += angle
         angles.append(cumulative_angle)
-    
-    # Ensure that angles meet the minimum angle constraint
-    min_angle = 0.00314159
-    for i in range(len(angles) - 1):
-        if angles[i + 1] - angles[i] < min_angle:
-            angles[i + 1] = angles[i] + min_angle
+
     
     # Return the calculated angles
     return {"instruments": angles}
